3ff5b1c4afde3c1e718a7d45ba0c5a9618aa4753
DavidGP201770 <davicho201170@gmail.com>
2018-03-20 21:44:12 -0600

    Aqui esta x4

---
+++ app/src/main/java/org/unitec/mensajitoandroid/MainActivity.java
@@ -0,0 +1,31 @@
+package org.unitec.mensajitoandroid;
+
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+    public class TareaBuscarMensajes extends AsyncTask<Void,Void,Void>{
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            return null;
+        }
+    }
+}
